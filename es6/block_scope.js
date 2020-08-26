4f3b6af873bd906d33ca989b3cd4c474c4296dfb
Eric <liyangzhong@hotmail.com>
2020-08-26 22:42:51 +0800

    init

---
+++ es6/block_scope.js
@@ -0,0 +1,77 @@
+// var person = "John";
+// console.log(person);
+// (function() {
+//     var person = "Jessica";
+//     console.log(person);
+// }())
+
+// console.log(person);
+
+// function check(bool) {
+//     if(bool) {
+//         var name = "Jessica";
+//         console.log(name);
+//     } else {
+//         console.log("Try again!");
+//     }
+//     console.log(name);
+// }
+// check(false);
+
+// var person = 'John'
+
+// console.log(person);
+
+// (function(){
+//     var person = 'Jessica'
+//     console.log(person)
+// }())
+
+// function check(bool){
+//     if(bool){
+//         var name = 'Jessica'
+//         console.log(name)
+//     } else {
+//         console.log('Try again!')
+//     }
+
+//     console.log(name)
+// }
+
+// check(false)
+
+// if(true) {
+//     let name = 'LeRoy'
+// }
+
+// console.log(name) 
+// if(true) {
+//     var name = 'LeRoy'
+// }
+
+// console.log(name) // => LeRoy
+
+// function sayHello(){
+//     let name = 'Jim'
+//     console.log(name)
+// }
+
+// sayHello()
+
+// let age = 99
+// {let age = 88}
+
+// var name = 'Andrew'
+
+// function foo(){
+//     let name = 'Jessica'
+//     if(true){
+//         let name = 'John'
+//         console.log(name)
+//     }
+//     console.log(name)
+// }
+
+// foo()
+
+// console.log(name)
